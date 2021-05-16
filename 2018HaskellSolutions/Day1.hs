7995b7c4117536ca506020750b4412fbe0cefe3b
Caleb Wilson <calebwilson706@gmail.com>
2021-05-16 12:42:15 +0100

    day 1 in haskell

--- 2018HaskellSolutions/Day1.hs
+++ 2018HaskellSolutions/Day1.hs
@@ -1,4 +1,29 @@
 module Day1 where
 
+import qualified Data.IntSet as Set
+
+day1FilePath = "/Users/calebjw/Documents/Developer/AdventOfCode/2018/Inputs/Day1Input.txt"
+
+getIntegerListFromStringList :: [String] -> [Int]
+getIntegerListFromStringList = map read
+
+getCircularIndex :: [a] -> Int -> Int
+getCircularIndex list index = rem index (length list)
+
+part1 :: [String] -> IO()
+part1 inputLines = print $ sum $ getIntegerListFromStringList inputLines
+
+part2 :: [String] -> IO()
+part2 inputLines = print(getFirstRepeatedFrequency(getIntegerListFromStringList inputLines, 0, Set.fromList [],0))
+
+
+getFirstRepeatedFrequency :: ([Int], Int, Set.IntSet, Int) -> Int
+getFirstRepeatedFrequency (offsets, currentIndex, visitedFrequencies, currentFrequency) =
+    if currentFrequency `Set.member` visitedFrequencies then currentFrequency else getFirstRepeatedFrequency(offsets,nextindex,updatedSet,updatedCurrentFrequency)
+       where nextindex = getCircularIndex offsets (currentIndex + 1)
+             updatedSet = Set.insert currentFrequency visitedFrequencies
+             updatedCurrentFrequency = currentFrequency + offsets!!currentIndex
+
 main :: IO()
-main = putStrLn "Hello Haskell World"
+main = do inputText <- readFile day1FilePath
+          part2 $ lines $ filter (/='+') inputText
